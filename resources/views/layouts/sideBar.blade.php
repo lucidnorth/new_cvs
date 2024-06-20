295c4781cddbd190e0287367addf6ec7a8d587c9
lucidnorth <146837488+lucidnorth@users.noreply.github.com>
2024-06-20 13:39:40 +0000

    sidebar logout fixed

--- resources/views/layouts/sideBar.blade.php
+++ resources/views/layouts/sideBar.blade.php
@@ -106,6 +106,23 @@
         <span class="nav-link-text ms-1">Talk to Us</span>
       </a>
     </li>
+
+    <li class="nav-item">
+    <a class="nav-link" href="#" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+        <div class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
+            <i class="ni ni-collection text-info text-sm opacity-10"></i>
+        </div>
+        <span class="nav-link-text ms-1">Log out</span>
+    </a>
+</li>
+
+<!-- Hidden form for logout -->
+<form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+    @csrf
+</form>
+        
+      </ul>
+    </div>
   </ul>
 </nav>
 
